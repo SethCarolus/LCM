ab685562f495e165746119b0830b2338d773511d
Seth Carolus <sethcarolus02@gmail.com>
2025-02-09 21:43:58 +0200

    Created Login Page UI

--- LCM_p.dpr
+++ LCM_p.dpr
@@ -2,13 +2,16 @@ program LCM_p;
 
 uses
   Vcl.Forms,
-  frmMain_u in 'frmMain_u.pas' {Form1};
+  frmMain_u in 'frmMain_u.pas' {frmMain},
+  Vcl.Themes,
+  Vcl.Styles;
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
-  Application.CreateForm(TForm1, Form1);
+  TStyleManager.TrySetStyle('Windows11 MineShaft');
+  Application.CreateForm(TfrmMain, frmMain);
   Application.Run;
 end.
